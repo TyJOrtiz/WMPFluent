404784e0935d5d67b9930c22c604fc3c91b9f6d8
Tyler Ortiz <imthewiseguy@outlook.com>
2023-06-06 18:46:16 -0700

    songcontrol

--- WMPFluent/Controls/AdaptiveSongControl.xaml.cs
+++ WMPFluent/Controls/AdaptiveSongControl.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WMPFluent.Models;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -19,9 +20,37 @@ namespace WMPFluent.Controls
 {
     public sealed partial class AdaptiveSongControl : UserControl
     {
+        public LibrarySong song { get { return this.DataContext as LibrarySong; } }
         public AdaptiveSongControl()
         {
             this.InitializeComponent();
+            this.DataContextChanged += (s, e) => this.Bindings.Update();
+            UpdateControlLayout(this.ActualWidth);
+            this.SizeChanged += AdaptiveSongControl_SizeChanged;
+        }
+
+        private void AdaptiveSongControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateControlLayout(e.NewSize.Width);
+        }
+
+        private void UpdateControlLayout(double width)
+        {
+            if (width < 648)
+            {
+                MainGrid.ColumnDefinitions[2].Width = new GridLength(0, GridUnitType.Pixel);
+                MainGrid.ColumnDefinitions[3].Width = new GridLength(0, GridUnitType.Pixel);
+            }
+            else if (width >= 648 && width < 720)
+            {
+                MainGrid.ColumnDefinitions[2].Width = new GridLength(0, GridUnitType.Pixel);
+                MainGrid.ColumnDefinitions[3].Width = new GridLength(48, GridUnitType.Pixel);
+            }
+            else if (width >= 720)
+            {
+                MainGrid.ColumnDefinitions[2].Width = new GridLength(1, GridUnitType.Star);
+                MainGrid.ColumnDefinitions[3].Width = new GridLength(48, GridUnitType.Pixel);
+            }
         }
     }
 }
